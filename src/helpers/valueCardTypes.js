c843883e89b69e54edfef8b5bc0a467b015e90f3
jf10rez <jflorezlondono@hotmail.com>
2022-07-26 11:13:17 -0500

    consts globals for validate upper and win

---
+++ src/helpers/valueCardTypes.js
@@ -0,0 +1,13 @@
+export const VALUE_OF_PINTS = {
+    HEARTS: 4,
+    SPADES: 3,
+    DIAMONDS: 2,
+    CLUBS: 1
+}
+
+export const VALUE_OF_TYPES = {
+    ACE: 14,
+    KING: 13,
+    QUEEN: 12,
+    JACK: 11
+}
