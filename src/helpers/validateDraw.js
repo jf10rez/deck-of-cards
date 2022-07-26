9b1b8f56b08254dedfc643daedda919ea10b5798
jf10rez <jflorezlondono@hotmail.com>
2022-07-26 11:13:48 -0500

    import valuos of types and pints

--- src/helpers/validateDraw.js
+++ src/helpers/validateDraw.js
@@ -1,19 +1,8 @@
+import { VALUE_OF_PINTS, VALUE_OF_TYPES } from "./valueCardTypes"
+
 export const validateDraw = ( player1 = {}, player2 = {} ) => {
 
     //1: gana jugador 1, 2: gana jugador 2
-    const VALUE_OF_PINTS = {
-        HEARTS: 4,
-        SPADES: 3,
-        DIAMONDS: 2,
-        CLUBS: 1
-    }
-
-    const VALUE_OF_TYPES = {
-        ACE: 14,
-        KING: 13,
-        QUEEN: 12,
-        JACK: 11
-    }
     
     if( VALUE_OF_PINTS[player1.suit] > VALUE_OF_PINTS[player2.suit] ) return 1
 
@@ -22,8 +11,6 @@ export const validateDraw = ( player1 = {}, player2 = {} ) => {
     if( (VALUE_OF_TYPES[player1.value] ?? +player1.value) > (VALUE_OF_TYPES[player2.value] ?? +player2.value) ){
         return 1
     }else{
-        console.log((VALUE_OF_TYPES[player1.value] ?? +player1.value))
-        console.log((VALUE_OF_TYPES[player2.value] ?? +player2.value))
         return 2
     }
 
